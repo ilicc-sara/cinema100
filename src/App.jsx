544f1a83668c9065f57323f7b3badc70263c8e2c
Sara <sarailic431@gmail.com>
2025-07-17 21:22:01 +0200

    ...............

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <div>
+          <img src="./dice-1.png" />
+          <img src="./dice-6.png" />
+        </div>
+        <button>Roll</button>
+      </main>
+    </>
+  );
 }
 
 export default App;
