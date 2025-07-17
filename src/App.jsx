ac6c8c92aceed30e7e87953ca39fa014ba22ef33
Sara <sarailic431@gmail.com>
2025-07-17 21:27:26 +0200

    ................

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [diceNumber, setDiceNumber] = useState([1, 1]);
+
+  const randomNum = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
   return (
     <>
       <main>
         <div>
-          <img src="./dice-1.png" />
-          <img src="./dice-6.png" />
+          <img src={`./dice-${diceNumber[0]}.png `} />
+          <img src={`./dice-${diceNumber[1]}.png`} />
         </div>
-        <button>Roll</button>
+        <button
+          onClick={() => setDiceNumber([randomNum(1, 6), randomNum(1, 6)])}
+        >
+          Roll
+        </button>
       </main>
     </>
   );
