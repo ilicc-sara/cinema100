3dfc3ef26576b0c7d319d0a230f05a17ffb36510
Sara <sarailic431@gmail.com>
2025-09-19 21:35:26 +0200

    . . . . . . .....

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,7 @@
 import { useState, useEffect } from "react";
 
 import "./App.css";
-import { loadConfigFromFile } from "vite";
 
-function App() {
-  const [resourceType, setResourceType] = useState("posts");
-  // const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    // fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-    //   .then((response) => response.json())
-    //   .then((json) => setItems(json));
-
-    console.log("resource changed");
-
-    return () => {
-      console.log("return from resource change");
-    };
-  }, [resourceType]);
-
-  return (
-    <>
-      <div className="btns">
-        <button onClick={() => setResourceType("posts")}>posts</button>
-        <button onClick={() => setResourceType("users")}>users</button>
-        <button onClick={() => setResourceType("comments")}>comments</button>
-      </div>
-      <h1> {resourceType} </h1>
-      {/* {items.map((item, index) => {
-        return <pre key={index}> {JSON.stringify(item)} </pre>;
-      })} */}
-    </>
-  );
-
-  //  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  // const handleResize = () => {
-  //   setWindowWidth(window.innerWidth);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("resize", handleResize);
-
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
-
-  // return (
-  //   <>
-  //     <div> {windowWidth} </div>
-  //   </>
-  // );
-}
+function App() {}
 
 export default App;
