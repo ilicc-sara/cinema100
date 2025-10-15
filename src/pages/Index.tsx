25dfb6a448ea3861b1e487692fb5b031054ccb16
Sara <sarailic431@gmail.com>
2025-10-15 23:56:14 +0200

    state inputs

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 
 function Home() {
   const [logIn, setLogIn] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <section className="min-h-screen !mt-[5%]">
       <div className="w-[fit-content] !mx-auto flex justify-center items-center gap-2">
@@ -15,11 +21,15 @@ function Home() {
             className="bg-[#e8f0fe] w-full rounded h-11 !pl-3"
             type="text"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="bg-[#e8f0fe] w-full rounded h-11 !pl-3"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="bg-[#fc4747] w-full rounded h-9 text-[#e8f0fe]">
             Log In
@@ -46,29 +56,37 @@ function Home() {
       )}
       {!logIn && (
         <form className="w-104 h-[fit-content] !px-14 !py-9 flex flex-col bg-[#161d2f] !mx-auto items-center gap-5 rounded-xl !my-5">
-          <h1 className="text-[#e8f0fe] text-3xl self-start">Log In</h1>
+          <h1 className="text-[#e8f0fe] text-3xl self-start">Sign Up</h1>
           <input
             className="bg-[#e8f0fe] w-full rounded h-11 !pl-3"
             type="text"
             placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             className="bg-[#e8f0fe] w-full rounded h-11 !pl-3"
             type="text"
             placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
           />
           <input
             className="bg-[#e8f0fe] w-full rounded h-11 !pl-3"
             type="text"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="bg-[#e8f0fe] w-full rounded h-11 !pl-3"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="bg-[#fc4747] w-full rounded h-9 text-[#e8f0fe]">
-            Log In
+            Sign Up
           </button>
 
           <div className="flex flex-col gap-2">
@@ -79,7 +97,7 @@ function Home() {
                 className="text-[#fc4747] cursor-pointer"
               >
                 {" "}
-                Log in
+                Log In
               </span>
             </p>
           </div>
