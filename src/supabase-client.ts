f7eb949b7aa282ff0be7ba85ca25e01b3ef33b3f
Sara <sarailic431@gmail.com>
2025-10-22 20:51:51 +0200

    environment

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import.meta.env.VITE_RAPID_API_KEY;
 
 export const supabase = createClient(
   "https://yyocycmzxqjdvkwqlpzd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5b2N5Y216eHFqZHZrd3FscHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0Nzg2MzEsImV4cCI6MjA3NjA1NDYzMX0.xiESB6vTdRhwuCLH4ArpMjfzSLGVljwgf9DNHZxb3FM"
+  import.meta.env.VITE_RAPID_API_KEY
 );
