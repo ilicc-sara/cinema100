0efdb6daa039e3dd2abb97f047d2310c8acaa3b6
Sara <sarailic431@gmail.com>
2025-10-25 20:31:51 +0200

    finishing off search bar

--- src/pages/homePage/Home.tsx
+++ src/pages/homePage/Home.tsx
@@ -125,7 +125,7 @@ function Home() {
         </div>
       </nav>
       <section>
-        <div className="bg-[#161d2f] w-[80%] !mx-auto rounded-xl !py-3 !px-5 !my-10">
+        <div className="bg-[#161d2f] w-[80%] !mx-auto rounded-xl !py-3 !px-5 !my-10 flex justify-between items-center">
           <div className="bg-[#bfbfbf] w-[fit-content] flex items-center justify-between !py-1 rounded-lg cursor-pointer active:shadow-[0_0_0_5px_rgb(252,71,71)] ">
             <input
               className="!pl-2 focus:outline-none focus:ring-0"
@@ -137,13 +137,23 @@ function Home() {
             </button>
           </div>
 
-          <div>
-            <select>
+          <div className="flex justify-between items-center gap-3">
+            <select className="bg-[#bfbfbf] rounded">
               <option>All</option>
               {genres.map((genre, index) => (
                 <option key={index}> {genre} </option>
               ))}
             </select>
+            <i className="bxr  bxs-bookmark"></i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#bfbfbf"
+              width="28"
+              height="28"
+            >
+              <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"></path>
+              <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"></path>
+            </svg>
           </div>
         </div>
         <div className=" grid grid-cols-4 w-[80%] !mx-auto gap-7 !px-5">
