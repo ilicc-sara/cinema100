e446394ea1272a6423e9921d0e147df2751640cd
Sara <sarailic431@gmail.com>
2025-11-18 18:41:18 +0100

    .......

--- src/pages/homePage/components/Movies.tsx
+++ src/pages/homePage/components/Movies.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useRef } from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import { useCallback } from "react";
 import MovieItem from "./MovieItem";
 import type { ActiveMoviesProps } from "../../../types";
 
