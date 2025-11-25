26ba4753f65cc71981c09426800cd6c1227980ff
Sara <sarailic431@gmail.com>
2025-11-25 17:14:09 +0100

    nav made responsive

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -2,10 +2,15 @@ import { Link } from "react-router";
 
 function Nav() {
   return (
-    <nav className="bg-brand-midnight flex justify-between items-center !px-20 !py-3">
-      <div className="w-[fit-content] flex justify-center items-center gap-2">
-        <img className="w-10 h-10" src="logo.png" />
-        <h1 className="text-white font-medium text-xl">cinema 100</h1>
+    <nav className="bg-brand-midnight flex justify-between items-center mobile:!px-20 max-mobile:!px-10 !py-3">
+      <div className="w-[fit-content] flex justify-center items-center mobile:gap-2 max-mobile:gap-1 xl:bg-red-400 desktop:bg-blue-400 laptop:bg-green-400 smallLT:bg-yellow-400 tablet:bg-fuchsia-400 mobile:bg-pink-400 smallmobile:bg-orange-400">
+        <img
+          className="mobile:w-10 mobile:h-10 max-mobile:w-9 max-mobile:w-9"
+          src="logo.png"
+        />
+        <h1 className="text-white font-medium tablet:text-xl mobile:text-lg max-mobile:hidden">
+          cinema 100
+        </h1>
       </div>
 
       <div className="text-[#e8f0fe] text-lg font-medium flex gap-10">
