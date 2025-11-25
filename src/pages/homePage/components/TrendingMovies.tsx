244f726a192da5f73e5d4072e325afdfb601e456
Sara <sarailic431@gmail.com>
2025-11-25 18:16:40 +0100

    simplify slides with functions

--- src/pages/homePage/components/TrendingMovies.tsx
+++ src/pages/homePage/components/TrendingMovies.tsx
@@ -1,8 +1,33 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import MovieItem from "./MovieItem";
-import type { TrendingProps } from "../../../types";
+import type { TrendingProps, singleMovie } from "../../../types";
 
 function TrendingMovies({ currentlyTrending }: TrendingProps) {
+  function TrendingMoviesSlide(
+    currentlyTrending: singleMovie[],
+    rangeIndex1: number,
+    rangeIndex2: number
+  ) {
+    if (rangeIndex2) {
+      return (
+        <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-[0px] w-[90%] !mx-auto">
+          {currentlyTrending.map((item: any, index: number) => {
+            if (index > rangeIndex1 && index <= rangeIndex2) {
+              return <MovieItem details={false} item={item} index={index} />;
+            }
+          })}
+        </div>
+      );
+    } else {
+      <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-3 w-[90%] !mx-auto">
+        {currentlyTrending.map((item, index) => {
+          if (index <= rangeIndex1) {
+            return <MovieItem details={false} item={item} index={index} />;
+          }
+        })}
+      </div>;
+    }
+  }
   return (
     <Splide
       className="currently-trending-slider"
@@ -15,7 +40,7 @@ function TrendingMovies({ currentlyTrending }: TrendingProps) {
     >
       <SplideSlide>
         {currentlyTrending && (
-          <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-[0px] w-[90%] !mx-auto">
+          <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-3 w-[90%] !mx-auto">
             {currentlyTrending.map((item, index) => {
               if (index <= 3) {
                 return <MovieItem details={false} item={item} index={index} />;
@@ -25,48 +50,16 @@ function TrendingMovies({ currentlyTrending }: TrendingProps) {
         )}
       </SplideSlide>
       <SplideSlide>
-        {currentlyTrending && (
-          <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-[0px] w-[90%] !mx-auto">
-            {currentlyTrending.map((item, index) => {
-              if (index > 3 && index <= 7) {
-                return <MovieItem details={false} item={item} index={index} />;
-              }
-            })}
-          </div>
-        )}
+        {currentlyTrending && TrendingMoviesSlide(currentlyTrending, 3, 7)}
       </SplideSlide>
       <SplideSlide>
-        {currentlyTrending && (
-          <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-[0px] w-[90%] !mx-auto">
-            {currentlyTrending.map((item, index) => {
-              if (index > 7 && index <= 11) {
-                return <MovieItem details={false} item={item} index={index} />;
-              }
-            })}
-          </div>
-        )}
+        {currentlyTrending && TrendingMoviesSlide(currentlyTrending, 7, 11)}
       </SplideSlide>
       <SplideSlide>
-        {currentlyTrending && (
-          <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-[0px] w-[90%] !mx-auto">
-            {currentlyTrending.map((item, index) => {
-              if (index > 11 && index <= 15) {
-                return <MovieItem details={false} item={item} index={index} />;
-              }
-            })}
-          </div>
-        )}
+        {currentlyTrending && TrendingMoviesSlide(currentlyTrending, 11, 15)}
       </SplideSlide>
       <SplideSlide>
-        {currentlyTrending && (
-          <div className="grid smallLT:grid-cols-4 max-smallLT:grid-cols-2 smallLT:gap-7 max-smallLT:gap-3 smallLT:!px-5 max-smallLT:!px-[0px] w-[90%] !mx-auto">
-            {currentlyTrending.map((item, index) => {
-              if (index > 15 && index <= 19) {
-                return <MovieItem details={false} item={item} index={index} />;
-              }
-            })}
-          </div>
-        )}
+        {currentlyTrending && TrendingMoviesSlide(currentlyTrending, 15, 19)}
       </SplideSlide>
     </Splide>
   );
