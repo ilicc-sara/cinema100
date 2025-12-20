959ea4b24b98d7351c2a423630f32add864298ac
Sara <sarailic431@gmail.com>
2025-12-20 22:20:02 +0100

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -10,50 +10,6 @@ import SharedLayout from "./layouts/SharedLayout.tsx";
 import SingleMovie from "./pages/singleMovie/Index.tsx";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
 
-// u "/" rutu proveriti postoji li access_token u local storage
-// ako ne postoji redirect na login
-// ako postoji ,
-
-// 1. pogledati postoji li user u stejtu
-// 2. ako postoji redirect na homepage
-// 3. ako ne postoji, dohvatiti ga na osnovu tokena
-// 4. staviti ga u stejt i mogu koristiti kontext
-// 5. redirect na homepage
-// ako sam dosla na homepage znaci da sam ulogovana
-
-// const router = createBrowserRouter([
-//   // {
-//   //   path: "/",
-//   //   element: <Navigate to="/login" replace />,
-//   // },
-//   {
-//     path: "/login",
-//     element: <AuthLogIn />,
-//   },
-//   {
-//     path: "/signup",
-//     element: <AuthSignUp />,
-//   },
-
-//   {
-//     element: (
-//       <PrivateRoute>
-//         <SharedLayout />
-//       </PrivateRoute>
-//     ),
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home />,
-//       },
-//       {
-//         path: "/movie/:movieId",
-//         element: <SingleMovie />,
-//       },
-//     ],
-//   },
-// ]);
-
 const router = createBrowserRouter([
   {
     path: "/login",
